465c21cc61da4a28377f4ed97aafc2328894c24d
vinhp <phat.vinh@brusselsairport.be>
2019-09-09 12:01:32 +0200

    first commit

--- initial/application/src/main/java/hello/app/Application.java
+++ initial/application/src/main/java/hello/app/Application.java
@@ -1,4 +1,27 @@
 package hello.app;
 
+import hello.service.MyService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication(scanBasePackages = "hello")
+@RestController
 public class Application {
+
+  private final MyService myService;
+
+  public Application(MyService myService) {
+    this.myService = myService;
+  }
+
+  @GetMapping("/")
+  public String home() {
+    return myService.message();
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
 }
--- initial/library/src/main/java/hello/service/ServiceProperties.java
+++ initial/library/src/main/java/hello/service/ServiceProperties.java
@@ -1,4 +1,13 @@
 package hello.service;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Getter
+@Setter
+@ConfigurationProperties("service")
 public class ServiceProperties {
+
+  private String message;
 }
